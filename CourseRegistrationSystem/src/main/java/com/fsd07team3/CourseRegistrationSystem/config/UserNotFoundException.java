19c7bc38a6dbac157b28c5cb2c13515f60fa9f6c
jean9947 <118857398+jean9947@users.noreply.github.com>
2023-09-04 14:48:31 -0400

    Delete CourseRegistrationSystem directory

--- CourseRegistrationSystem/src/main/java/com/fsd07team3/CourseRegistrationSystem/config/UserNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.fsd07team3.CourseRegistrationSystem.config;
-
-public class UserNotFoundException extends RuntimeException {
-
-    public UserNotFoundException(String message) {
-        super(message);
-    }
-}
